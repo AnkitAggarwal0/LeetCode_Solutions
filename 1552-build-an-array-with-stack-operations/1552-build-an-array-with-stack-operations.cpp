cf3c16de01e691b49c13246db5fd2073ce3b9a01
Ankit Aggarwal <74857457+AnkitAggarwal0@users.noreply.github.com>
2026-01-29 18:03:45 -0500

    1552-BUILD-AN-ARRAY-WITH-STACK-OPERATIONS Stats: Time: 0 ms (100%), Space: 10.8 MB (19.49%) - LeetHub

--- 1552-build-an-array-with-stack-operations/1552-build-an-array-with-stack-operations.cpp
+++ 1552-build-an-array-with-stack-operations/1552-build-an-array-with-stack-operations.cpp
@@ -4,14 +4,10 @@ public:
         int curr_stream_num = 1; 
         int target_idx = 0; 
         vector<string> result;
-        while (target_idx < target.size()){
+        while (target_idx < target.size() && curr_stream_num <= n){
             result.push_back("Push");
-            if (curr_stream_num == target[target_idx]){
-                target_idx++;
-            }
-            else{
-                result.push_back("Pop");
-            }
+            if (curr_stream_num == target[target_idx]) target_idx++;
+            else result.push_back("Pop");
             curr_stream_num++;
         }
         return result;
