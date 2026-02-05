7be7e5312293fc91ffdbc9b72c7274066e7da9e2
Ankit Aggarwal <74857457+AnkitAggarwal0@users.noreply.github.com>
2026-02-04 22:23:50 -0500

    0373-FIND-K-PAIRS-WITH-SMALLEST-SUMS Stats: Time: 31 ms (96.13%), Space: 134.3 MB (84.96%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -5,14 +5,15 @@ public:
         typedef pair<int, pair<int,int>> Element; 
         std::priority_queue<Element, vector<Element>, std::greater<Element>> pq; 
 
-        for (int i = 0; i<nums1.size(); i++){
+        for (int i = 0; i<nums1.size() && i<k; i++){
             pq.push({nums1[i]+nums2[0], {i, 0}});
         } 
 
         vector<vector<int>> result; 
         while(k-- > 0 && !pq.empty()){
-            int i = pq.top().second.first;
-            int j = pq.top().second.second;
+            auto top = pq.top();
+            int i = top.second.first;
+            int j = top.second.second;
             result.push_back({nums1[i], nums2[j]});
             pq.pop();
 
